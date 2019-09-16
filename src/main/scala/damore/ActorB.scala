39eaea061935162279a85990eb45147593780e0d
Vincenzo D'Amore <v.damore@gmail.com>
2019-09-16 10:53:24 +0200

    wip

--- src/main/scala/damore/ActorB.scala
+++ src/main/scala/damore/ActorB.scala
@@ -1,10 +1,11 @@
 package damore
 
-import akka.actor.{Actor, Props}
+import akka.actor.{Actor, ActorRef, Props}
 import akka.event.Logging
-import akka.pattern.{Patterns, RetrySupport}
+import akka.pattern.{AskTimeoutException, Patterns}
 import akka.util.Timeout
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
 object ActorB {
@@ -30,22 +31,39 @@ class ActorB(actorCProps: Props) extends Actor {
 
   val actorC = context.actorOf(actorCProps)
 
+  def retry(actorRef: ActorRef, message: Any, maxAttempts: Int): Future[Any] = {
+     retry(actorRef, message, maxAttempts, 1)
+  }
+
+  def retry(actorRef: ActorRef, message: Any, maxAttempts: Int, attempt: Int): Future[Any] = {
+    log.info("ActorB - sent message MessageB2C to ActorC " + actorC)
+    val future = Patterns.ask(actorRef, message, 50.millisecond) recover {
+      case e: AskTimeoutException =>
+        if (attempt <= maxAttempts) retry(actorRef, message, maxAttempts, attempt + 1)
+        else None // Return default result according to your requirement, if actor is non-reachable.
+    }
+    future
+  }
+
   def receive = {
     case r:MessageB2C_Ack => {
       log.info("ActorB - secondary received UNHANDLED message: MessageB2C_Ack")
     }
     case r:MessageA2B => {
       val client = context.sender()
-      implicit val timeout = Timeout(50.milliseconds)
+      implicit val timeout = Timeout(100.milliseconds)
       log.info("ActorB received message MessageA2B from client " + client)
       implicit val scheduler=context.system.scheduler
       val p = MessageB2C()
 
-      RetrySupport.retry(() => {
-        log.info("ActorB - sent message MessageB2C to ActorC " + actorC)
-        Patterns.ask(actorC, p, 50.millisecond)
-      }, 10, 50.millisecond)
-      .onSuccess({
+      //Return a new future that will retry up to 10 times
+//      val retried = akka.pattern.retry(() => attempt(actorC), 10, 100 milliseconds)
+
+//      val retried = RetrySupport.retry(() => {
+//        log.info("ActorB - sent message MessageB2C to ActorC " + actorC)
+//        Patterns.ask(actorC, p, 50.millisecond)
+//      }, 6, 0.millisecond)
+      retry(actorC, p, 10) onSuccess({
         case p: MessageB2C_Ack => {
           log.info("ActorB - Received MessageB2C_Ack so now sending an MessageA2B_Ack to client " + client)
           client ! MessageA2B_Ack()
